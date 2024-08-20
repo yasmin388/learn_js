f6f4735c207492d4958d0786e9f58c23a28a1e47
yasmin388 <153319501+yasmin388@users.noreply.github.com>
2024-08-20 11:42:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+let markUp=`
+<div class="card">
+<div class="child">
+<h3>Welcome elzero</h3>
+<p>elzero web school</p>
+<span> 25/10 </span>
+</div>
+</div>
+`;
+document.write(markUp);
+let n=`
+<div class="card">
+<div class="child">
+<h3>Welcome elzero</h3>
+<p>elzero web school</p>
+<span> 25/10 </span>
+</div>
+</div>
+`;
+document.write(n);
+let p=`
+<div class="card">
+<div class="child">
+<h3>Welcome elzero</h3>
+<p>elzero web school</p>
+<span> 25/10 </span>
+</div>
+</div>
+`;
+document.write(p);
+let m=`
+<div class="card">
+<div class="child">
+<h3>Welcome elzero</h3>
+<p>elzero web school</p>
+<span> 25/10 </span>
+</div>
+</div>
+`;
+document.write(m);
